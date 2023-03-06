bf20f505ef7ea703b53b85cb11f9fa97372c92ce
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-03-06 23:11:55 +0000

    lab1 implemented

--- lab1/src/TcpClient.java
+++ lab1/src/TcpClient.java
@@ -1,30 +1,29 @@
-package src;
-
-import java.net.* ;
-import java.util.*;
+import java.net.Socket;
+import java.net.URI;
+import java.util.List;
+import java.util.Scanner;
 
 /**
- * Basic TCP client... 
- *
+ * Basic TCP client...
  */
 public class TcpClient {
-    
+	
 	private static final String QUIT = "!quit";
-
+	
 	public static void main(String[] args) throws Exception {
-        
-    	// Use Discovery to obtain the hostname and port of the server;
-    	var port = -1;
-    	var hostname = ""; 
-    	
-
-    	try( var cs = new Socket( hostname, port); var sc = new Scanner(System.in)) {
-    		String input;
-    		do {
-    			input = sc.nextLine();
-    			cs.getOutputStream().write( (input + System.lineSeparator()).getBytes() );
-    		} while( ! input.equals(QUIT));
-    		
-    	}
-    }  
+		URI uri = DiscoverySingleton.getInstance().knownURIsOf("service1", 1).get(0);
+		String hostname = uri.getHost();
+		int port = uri.getPort();
+		
+		System.out.println("Connected with server at: " + uri);
+		
+		try (var cs = new Socket(hostname, port); var sc = new Scanner(System.in)) {
+			String input;
+			do {
+				input = sc.nextLine();
+				cs.getOutputStream().write((input + System.lineSeparator()).getBytes());
+			} while (!input.equals(QUIT));
+			
+		}
+	}
 }
--- lab1/src/TcpServer.java
+++ lab1/src/TcpServer.java
@@ -1,36 +1,34 @@
-package src;
-
-import java.net.* ;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
- * Basic TCP server... 
- *
+ * Basic TCP server...
  */
 public class TcpServer {
-    
-    private static final int PORT = 9000;
+	
+	private static final int PORT = 9000;
 	private static final int BUF_SIZE = 1024;
-
+	private static final String URI_FORMAT = "tcp://%s:%s";
+	
 	public static void main(String[] args) throws Exception {
-        
-		// Use Discovery to announce the uri of this server, in the form of (tcp://hostname:port)
-
-        
-		try(var ss = new ServerSocket( PORT )) {
-			System.err.println("Accepting connections at: " + ss.getLocalSocketAddress() ) ;
-            while( true ) {
-                var cs = ss.accept() ;
-                
-                System.err.println("Accepted connection from client at: " + cs.getRemoteSocketAddress() ) ;
-                
-                int n;
-                var buf = new byte[ BUF_SIZE];
-                while( (n = cs.getInputStream().read(buf)) > 0)
-                	System.out.write( buf, 0, n);
-                
-                System.err.println("Connection closed.") ;
-            }        	
-        }
-    }
-    
+		String uri = String.format(URI_FORMAT, InetAddress.getLocalHost().getHostAddress(), PORT);
+		DiscoverySingleton.getInstance().announce("service1", uri);
+		System.out.println("Accepting connections at: " + uri);
+		
+		try (var ss = new ServerSocket(PORT)) {
+			while (true) {
+				Socket cs = ss.accept();
+				System.out.println("Accepted connection from client at: " + cs.getRemoteSocketAddress());
+				
+				byte[] buf = new byte[BUF_SIZE];
+				int bytesRead;
+				while ((bytesRead = cs.getInputStream().read(buf)) > 0)
+					System.out.write(buf, 0, bytesRead);
+				
+				System.err.println("Connection closed.");
+			}
+		}
+	}
+	
 }
