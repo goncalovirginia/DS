b8260c7ea2361efb51f8734185ab726c9d5b2289
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-04-13 17:55:41 +0100

    project1 refactored Client

--- project1/src/clients/Client.java
+++ project1/src/clients/Client.java
@@ -1,5 +1,6 @@
 package clients;
 
+import api.java.Result;
 import jakarta.ws.rs.ProcessingException;
 import jakarta.xml.ws.WebServiceException;
 
@@ -11,14 +12,8 @@ public abstract class Client {
 	protected static final Logger Log = Logger.getLogger(Client.class.getName());
 	protected static final int READ_TIMEOUT = 10000, CONNECT_TIMEOUT = 10000, RETRY_SLEEP = 1000, MAX_RETRIES = 3;
 	
-	private int retries = MAX_RETRIES;
-	
-	public void setRetries(int retries) {
-		this.retries = retries;
-	}
-	
 	protected <T> T reTry(Supplier<T> func) {
-		for (int i = 0; i < retries; i++) {
+		for (int i = 0; i < MAX_RETRIES; i++) {
 			try {
 				return func.get();
 			}
